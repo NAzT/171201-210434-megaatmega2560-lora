83e89595e71fc03ccd26bbe3c885bd38d102c6a6
Nat <nat@cmmc.io>
2017-12-17 15:12:41 +0700

    remove unused.

--- src/CMMC_LoRa.ino
+++ src/CMMC_LoRa.ino
@@ -34,14 +34,13 @@ static void GPSloop()
 {
   while (gps.available( gpsPort ))
     doSomeWork( gps.read() );
-} // GPSloop
+} // GPSloop 
 
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-String udpData = "";
 CMMC_Interval interval;
+
 #include <CMMC_RX_Parser.h>
 #include "packet.h"
+
 void array_to_string(byte array[], unsigned int len, char buffer[])
 {
   for (unsigned int i = 0; i < len; i++)
@@ -56,51 +55,28 @@ void array_to_string(byte array[], unsigned int len, char buffer[])
 CMMC_RX_Parser parser(&Serial3);
 
 bool flag_dirty = false;
-char bbb[400];
 
 static CMMC_MASTER_PACKET_T master_packet;
-
-static void msg_recv(u8 * packet, u8 len) {
-  Serial.println("RECV PACKET..");
-  Serial.println();
-  Serial.print(F("packet size= "));
-  Serial.println(len);
-
-  Serial.println("MASTER_PACKET..");
-  Serial.print("master_packet size= ");
-  Serial.println(sizeof(master_packet));
-  if (len == sizeof(master_packet.packet)) {
-    memcpy(&(master_packet.packet), packet, len);
-
-    Serial.println(String("project = ") + master_packet.packet.project);
-    Serial.println(String("version = ") + master_packet.packet.version);
-    Serial.println(String("field1 = ")  + master_packet.packet.data.field1);
-    Serial.println(String("field2 = ")  + master_packet.packet.data.field2);
-    Serial.println(String("battery = ") + master_packet.packet.data.battery);
-    Serial.println(String("myName= ") + master_packet.packet.data.myName);
-
-    flag_dirty = true;
-  }
-}
 
 void setup()
 {
   Serial.begin(57600);
-  Serial.println("Initalizing LoRa module...");
-  Serial3.begin(57600);
   gpsPort.begin(9600);
+  Serial2.begin(57600);
+
+  Serial.println("Initalizing LoRa module...");
   Serial.println("BEGIN...");
-  parser.on_command_arrived(&msg_recv);
 }
 
-String hexString;
 void loop()
 {
   parser.process();
   GPSloop();
     interval.every_ms(2L * 1000, []() { 
       Serial.println(millis());
-      Serial.print(gps_latitude); Serial.print(","); Serial.println(gps_longitude);
+      Serial.print(gps_latitude); 
+      Serial.print(","); 
+      Serial.println(gps_longitude);
     });
 }
 
